8d9403f48b0d41d9c01eb6a4c19ae6039e2b5bf3
Bart Prokop <prokop.bart@gmail.com>
2017-01-31 22:55:58 +0000

    Adding swagger support

--- src/main/java/name/prokop/bart/config/WebMvcConfiguration.java
+++ src/main/java/name/prokop/bart/config/WebMvcConfiguration.java
@@ -1,9 +1,17 @@
 package name.prokop.bart.config;
 
 import java.util.logging.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  *
@@ -11,7 +19,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
+@EnableSwagger2
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     private static final Logger LOGGER = Logger.getLogger(WebMvcConfiguration.class.getName());
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        LOGGER.info("Configure additional ResourceHandlers");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        LOGGER.info("Configure additional ViewControllers");
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
+
 }
